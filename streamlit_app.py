70271ce86cbde041b0f9c47dfa26bafb825ca771
feb71 <150591456+feb71@users.noreply.github.com>
2024-10-12 17:01:07 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,21 +35,22 @@ def extract_data_from_pdf(file, doc_type, invoice_number=None):
                 
                 lines = text.split('\n')
                 for line in lines:
-                    if doc_type == "Faktura" and "Artikkel" in line or "Nummer" in line:
+                    if doc_type == "Faktura" and "Artikkel" in line:
                         start_reading = True
                         continue
 
                     if start_reading:
                         columns = line.split()
-                        if len(columns) >= 5:
+                        if len(columns) >= 6:
                             item_number = columns[1]
                             if not item_number.isdigit():
-                                continue
+                                continue  # Skipper fakturanummeret
 
-                            description = " ".join(columns[2:-3])
+                            description = " ".join(columns[2:-4])
                             try:
-                                quantity = float(columns[-3].replace('.', '').replace(',', '.')) if columns[-3].replace('.', '').replace(',', '').isdigit() else columns[-3]
-                                unit_price = float(columns[-2].replace('.', '').replace(',', '.')) if columns[-2].replace('.', '').replace(',', '').isdigit() else columns[-2]
+                                quantity = float(columns[-4].replace('.', '').replace(',', '.')) if columns[-4].replace('.', '').replace(',', '').isdigit() else columns[-4]
+                                unit_price = float(columns[-3].replace('.', '').replace(',', '.')) if columns[-3].replace('.', '').replace(',', '').isdigit() else columns[-3]
+                                discount = float(columns[-2].replace('.', '').replace(',', '.')) if columns[-2].replace('.', '').replace(',', '').isdigit() else columns[-2]
                                 total_price = float(columns[-1].replace('.', '').replace(',', '.')) if columns[-1].replace('.', '').replace(',', '').isdigit() else columns[-1]
                             except ValueError as e:
                                 st.error(f"Kunne ikke konvertere til flyttall: {e}")
@@ -58,10 +59,11 @@ def extract_data_from_pdf(file, doc_type, invoice_number=None):
                             unique_id = f"{invoice_number}_{item_number}" if invoice_number else item_number
                             data.append({
                                 "UnikID": unique_id,
-                                "Varenummer": item_number,  # Bruk Varenummer som det konsoliderte navnet
+                                "Varenummer": item_number,
                                 "Beskrivelse_Faktura": description,
                                 "Antall_Faktura": quantity,
                                 "Enhetspris_Faktura": unit_price,
+                                "Rabatt_Faktura": discount,
                                 "Totalt pris": total_price,
                                 "Type": doc_type
                             })
@@ -78,10 +80,8 @@ def split_description(data, doc_type):
     if doc_type == "Faktura":
         data['Enhet_Faktura'] = data['Beskrivelse_Faktura'].str.extract(r'(\bM2|\bM|\bSTK)$', expand=False)
         data['Beskrivelse_Faktura'] = data['Beskrivelse_Faktura'].str.replace(r'\s*\b(M2|M|STK)$', '', regex=True)
-
         data['Antall_Faktura'] = data['Beskrivelse_Faktura'].str.extract(r'(\d+)$', expand=False).astype(float)
         data['Beskrivelse_Faktura'] = data['Beskrivelse_Faktura'].str.replace(r'\s*\d+$', '', regex=True)
-    
     return data
 
 # Funksjon for å konvertere DataFrame til en Excel-fil
@@ -94,29 +94,6 @@ def convert_df_to_excel(df):
 # Hovedfunksjon for Streamlit-appen
 def main():
     st.title("Sammenlign Faktura mot Tilbud")
-    # Justerer tykkelsen på kolonneoverskriftene
-    st.markdown("""
-    <style>
-        .dataframe th {
-            font-weight: bold !important;  /* Gjør kolonneoverskriftene fet */
-        }
-    </style>
-    """, unsafe_allow_html=True)
-
-    # Justerer størrelsen, fargen og bakgrunnsfargen på kolonneoverskriftene
-    st.markdown(
-    """
-    <style>
-    .css-1n76uvr thead th {
-        font-size: 36px !important;  /* Justerer skriftstørrelsen */
-        color: #FFFFFF !important;   /* Endrer fargen til hvit */
-        background-color: #333333 !important;  /* Justerer bakgrunnsfargen */
-    }
-    </style>
-    """, 
-    unsafe_allow_html=True)
-
-
 
     # Opprett tre kolonner
     col1, col2, col3 = st.columns([1, 5, 1])
@@ -148,7 +125,7 @@ def main():
 
             # Riktige kolonnenavn fra Excel-filen for tilbud
             offer_data.rename(columns={
-                'VARENR': 'Varenummer',  # Bruk Varenummer som felles betegnelse
+                'VARENR': 'Varenummer',
                 'BESKRIVELSE': 'Beskrivelse_Tilbud',
                 'ANTALL': 'Antall_Tilbud',
                 'ENHET': 'Enhet_Tilbud',
@@ -191,7 +168,7 @@ def main():
                     st.dataframe(only_in_invoice)
 
                 # Lagre kun artikkeldataene til XLSX
-                all_items = invoice_data[["UnikID", "Varenummer", "Beskrivelse_Faktura", "Antall_Faktura", "Enhetspris_Faktura", "Totalt pris"]]
+                all_items = invoice_data[["UnikID", "Varenummer", "Beskrivelse_Faktura", "Antall_Faktura", "Enhetspris_Faktura", "Rabatt_Faktura", "Totalt pris"]]
                 
                 excel_data = convert_df_to_excel(all_items)
 
@@ -209,14 +186,16 @@ def main():
                         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                     )
 
-                    # Lag en Excel-fil med varenummer som finnes i faktura, men ikke i tilbud
+                  
+                     # Lag en Excel-fil med varenummer som finnes i faktura, men ikke i tilbud
                     only_in_invoice_data = convert_df_to_excel(only_in_invoice)
                     st.download_button(
-                        label="Last ned varenummer som ikke eksiterer i tilbudet",
+                        label="Last ned varenummer som ikke eksisterer i tilbudet",
                         data=only_in_invoice_data,
                         file_name="varer_kun_i_faktura.xlsx",
                         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                     )
+
             else:
                 st.error("Kunne ikke lese tilbudsdata fra Excel-filen.")
         else:
